d56f2044213478b5fb67ca63d1dc2e6ebb190fb1
mikasesmith <clamikasmith@gmail.com>
2021-05-20 11:17:47 -0500

    Form

---
+++ src/Form.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Form = (props) => {
+  //STATE FOR THE FORM
+  const [formData, setFormData] = React.useState(props.song);
+
+  //FUNCTIONS
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent Form from Refreshing
+    props.handleSubmit(formData); // Submit to Parents desired function
+    props.history.push("/"); //Push back to display page
+  };
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="artist"
+        value={formData.artist}
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        name="time"
+        value={formData.time}
+        onChange={handleChange}
+      />
+      <input type="submit" value={props.label} />
+    </form>
+  );
+};
+
+export default Form;
