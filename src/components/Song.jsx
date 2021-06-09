fb241b0a3691acb7c909f270f9bf61e145b7960f
ben.norrichs@gmail.com <ben.norrichs@gmail.com>
2021-06-08 22:05:19 -0400

    update to new aws backend

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -5,7 +5,7 @@ const Song = (props) => {
 		<section onClick={()=>{props.handleUpdateClick(props.song)}}className="song-component">
 			<span>{props.song.title}</span>
 			<span>{props.song.artist}</span>
-			<span>{props.song.time}</span>
+			<span>{props.song.length}</span>
 			<button onClick={()=>{props.handleDelete(props.song)}}>Delete</button>
 			<button onClick={()=>{props.handleAddPlay(props.song)}}>Play</button>
 		</section>
